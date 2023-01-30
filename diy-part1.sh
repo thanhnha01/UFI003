d4890ab3ef2fedfb4a3303d3ed9d849075bdb06e
jxw7453 <jxw7453@163.com>
2023-01-30 20:36:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -26,7 +26,5 @@ echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git;master' >>
 echo 'src-git lede https://github.com/coolsnowwolf/lede.git;master' >>feeds.conf.default
 echo 'src-git ntfs3 https://github.com/xymopen/openwrt-ntfs3.git;master' >>feeds.conf.default
 echo 'src-git netnasemusic https://github.com/maxlicheng/luci-app-unblockmusic.git;master' >>feeds.conf.default
-# echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
-# echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
 # echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git;master' >>feeds.conf.default
 # echo 'src-git lucie https://github.com/openwrt/luci.git' >>feeds.conf.default
