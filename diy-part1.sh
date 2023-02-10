e439975a7bd3579b24ef1b24746ed08be7a35927
jxw7453 <jxw7453@163.com>
2023-02-10 23:31:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,15 +16,14 @@ sed -i '10s/^/#/' feeds.conf.default
 
 # Add a feed source
 # echo 'src-git ' >>feeds.conf.default
-echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git;main' >> feeds.conf.default
 # echo 'src-git small https://github.com/kenzok8/small.git;master' >> feeds.conf.default
+echo 'src-git small8 https://github.com/kenzok8/small-package;master' >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 echo 'src-git theme_argon https://github.com/jerrykuku/luci-theme-argon.git;master' >> feeds.conf.default
 echo 'src-git nas_istoreos https://github.com/istoreos/istoreos.git;main' >> feeds.conf.default
 echo 'src-git nas_istore https://github.com/linkease/istore.git;main' >> feeds.conf.default
 echo 'src-git nas_isp https://github.com/linkease/istore-packages.git;main' >> feeds.conf.default
-# echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git;master' >> feeds.conf.default
 echo 'src-git lede https://github.com/coolsnowwolf/lede.git;master' >>feeds.conf.default
 echo 'src-git ntfs3 https://github.com/xymopen/openwrt-ntfs3.git;master' >>feeds.conf.default
 echo 'src-git netnasemusic https://github.com/maxlicheng/luci-app-unblockmusic.git;master' >>feeds.conf.default
